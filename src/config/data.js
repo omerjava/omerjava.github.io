01cf6e177277507284504499f40675b3ed8300b9
Omer Kala <omer.kala@vito.be>
2024-12-07 00:27:58 +0100

    feat: add new skills and projects

--- src/config/data.js
+++ src/config/data.js
@@ -12,56 +12,86 @@ import chatO from "../assets/chat-o.png";
 import myPortfolio from "../assets/portfolio-website.png";
 import askYourQuestion from "../assets/Ask-Your-Question.jpg";
 import employeeManagementSystem from "../assets/Employee-Management-System.jpg";
+import wig from "../assets/wig.png";
+import mapeo from "../assets/mapeo.png";
+import terrascope from "../assets/terrascope.png";
 
 
 export const myProjects = [
   {
     id: 1,
+    type: "work experience",
+    project: "WatchItGrow",
+    link: "http://www.watchitgrow.be",
+    photo: wig,
+    info: "Angular-Spring-ElasticSearch-Python(Flask-FastApi)-Apache Nifi-Cypress",
+    description: "Contributed to WatchItGrow project between Mar 2023-Mar 2025, completed tasks on Angular FE, Java and Python RestApi services and database, wrote tests with Cypress at FE.",
+  },
+  {
+    id: 2,
+    type: "work experience",
+    project: "Mapeo",
+    link: "http://mapeo.be",
+    photo: mapeo,
+    info: "Angular-Node.js Serverless(AWS)-PostgreSQL-Cypress",
+    description: "Took part in FE and BE development and testing between Feb 2024 and Feb 2025",
+  },
+  {
+    id: 3,
+    type: "work experience",
+    project: "Terrascope Viewer",
+    link: "http://viewer.terrascope.be",
+    photo: terrascope,
+    info: "Angular-Nest.js-ElasticSearch",
+    description: "Helped to maintenance works and fixing bugs at FE between Jan 2024-Nov 2024",
+  },
+  {
+    id: 4,
     type: "fullstack",
     project: "Ask Your Question",
     link: "http://ask-your-question-app.s3-website.eu-central-1.amazonaws.com/",
     photo: askYourQuestion,
     info: "React-Spring-MySQL",
-    description: "",
+    description: "AWS deployment is not active!",
   },
   {
-    id: 2,
+    id: 5,
     type: "fullstack",
     project: "Employee Management System",
     link: "http://angular-employee-app.s3-website.eu-central-1.amazonaws.com",
     photo: employeeManagementSystem,
     info: "Angular-Spring-MySQL",
-    description: "",
+    description: "AWS deployment is not active!",
   },
   {
-    id: 3,
+    id: 6,
     type: "fullstack",
     project: "Awesome Chat App",
     link: "https://omerjava.github.io/chat-o-react/",
     photo: chatO,
     info: "React-Strapi",
-    description: "",
+    description: "Backend that is deployed in Heroku is not active!",
   },
   {
-    id: 4,
+    id: 7,
     type: "fullstack",
     project: "BugFinders",
     link: "https://omerjava.github.io/bug-finders-frontend/",
     photo: bugFinders,
     info: "React-NodeJs-Express-PostgreSQL",
-    description: "",
+    description: "Express.js backend that is deployed in Heroku is not active!",
   },
   {
-    id: 5,
+    id: 8,
     type: "fullstack",
     project: "Hack Your Film",
     link: "https://omerjava.github.io/hack-your-film/",
     photo: hackYourFilm,
     info: "React-Java Spring-PostgreSQL",
-    description: "",
+    description: "Java Spring backend that is deployed in Heroku is not active!",
   },
   {
-    id: 6,
+    id: 9,
     type: "frontend",
     project: "Antwerp Pizza",
     link: "https://omerjava.github.io/antwerp-pizza/",
@@ -70,7 +100,7 @@ export const myProjects = [
     description: "",
   },
   {
-    id: 7,
+    id: 10,
     type: "frontend",
     project: "My Portfolio Website",
     link: "https://omerjava.github.io",
@@ -79,7 +109,7 @@ export const myProjects = [
     description: "",
   },
   {
-    id: 8,
+    id: 11,
     type: "frontend",
     project: "Global Challenges",
     link: "https://global-challenges-website.glitch.me",
@@ -88,7 +118,7 @@ export const myProjects = [
     description: "",
   },
   {
-    id: 9,
+    id: 12,
     type: "frontend",
     project: "Angular Team Generator",
     link: "https://omerjava.github.io/angular-team-generator/",
@@ -97,7 +127,7 @@ export const myProjects = [
     description: "",
   },
   {
-    id: 10,
+    id: 13,
     type: "frontend",
     project: "Angular Todo App",
     link: "https://omerjava.github.io/angular-todo/",
@@ -106,7 +136,7 @@ export const myProjects = [
     description: "",
   },
   {
-    id: 11,
+    id: 14,
     type: "frontend",
     project: "React Google Clone",
     link: "https://omerjava.github.io/react-google-clone/",
@@ -115,7 +145,7 @@ export const myProjects = [
     description: "",
   },
   {
-    id: 12,
+    id: 15,
     type: "frontend",
     project: "React Netflix Clone",
     link: "https://omerjava.github.io/react-netflix-clone/",
@@ -125,7 +155,7 @@ export const myProjects = [
   },
 
   {
-    id: 13,
+    id: 16,
     type: "frontend",
     project: "React Calculator",
     link: "https://omerjava.github.io/react-calculator/",
@@ -134,7 +164,7 @@ export const myProjects = [
     description: "",
   },
   {
-    id: 14,
+    id: 17,
     type: "frontend",
     project: "React Todo",
     link: "https://omerjava.github.io/react-todo-app/",
@@ -147,19 +177,19 @@ export const myProjects = [
 export const mySkills = [
   {
     area: "Frontend",
-    details: "HTML, CSS, JavaScript, TypeScript, React, Angular, Bootstrap",
+    details: "Angular, React, NgRx, RxJs, JavaScript, TypeScript, Cypress",
   },
   {
     area: "Backend",
-    details: "Spring Boot, Spring JPA, Spring JDBC, Spring Security, RestAPI, Node.js, Express.js, Json Web Tokens, Strapi",
+    details: "Java Spring, Hibernate, Spring JPA, JDBC, Apache Nifi, RestAPI, Node.js, Express.js, Flask, FastAPI",
   },
   {
     area: "Database",
-    details: "MySQL, PostgreSQL",
+    details: "MySQL, PostgreSQL, ElasticSearch",
   },
   {
     area: "Prog.Language",
-    details: "Java, JavaScript, TypeScript, SQL",
+    details: "Java, JavaScript, Python, TypeScript",
   },
   {
     area: "Education",
